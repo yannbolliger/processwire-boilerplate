4dfe3fe2242135746e5b1e38074582cb52118484
Yann Bolliger <ybstudio@windowslive.com>
2018-01-18 02:53:18 +0000

    Clean more.

--- site/templates/_init.php
+++ site/templates/_init.php
@@ -15,7 +15,7 @@
 // Here we also assign default values for each of them.
 $title = $page->get('title');
 $content = $page->body;
-$sidebar = $page->sidebar;
+$sidebar = "";
 
 // We refer to our homepage a few times in our site, so
 // we preload a copy here in $homepage for convenience.
