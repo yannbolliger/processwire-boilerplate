cb75012007cc5640983dd8387097cd803aaf4aca
Yann Bolliger <ybstudio@windowslive.com>
2018-01-18 03:15:04 +0000

    Add normalize and SASS workflow.

--- site/templates/_main.php
+++ site/templates/_main.php
@@ -113,5 +113,8 @@
 	<footer>
 	</footer>
 
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="<?php echo $config->urls->templates?>scripts/main.js" ></script>
+
 </body>
 </html>
