702a9faf44a538f8d04f79b51398887c0856e17a
Yann Bolliger <ybstudio@windowslive.com>
2019-03-21 23:26:19 +0100

    Modernise js slightly.

--- site-bs/templates/scripts/main.js
+++ site-bs/templates/scripts/main.js
@@ -4,87 +4,91 @@ $(function() {
    */
   /*
   $("#menu #burger").click(function() {
-    $(".topnav").toggleClass("hidden");
-  });
+    $(".topnav").toggleClass("hidden")
+  })
   */
   /**
    * Scrolls to contact form on click in menu
    */
   /*
   $("#menu a[href*=contact]").click(function(e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: $("#contact").offset().top }, 1000);
-  });
+    e.preventDefault()
+    $("html, body").animate({scrollTop: $("#contact").offset().top}, 1000)
+  })
   */
   /**
    *  Colorbox
    */
   /*
-  $('a.colorbox').colorbox({
-    rel:'foto',
-    transition: 'fade',
+  $("a.colorbox").colorbox({
+    rel: "foto",
+    transition: "fade",
     fixed: true,
-    opacity: '0.85',
-    maxWidth: '100%',
-    maxHeight: '100%',
-    initialWidth: '300',
-    initialHeight: '100',
+    opacity: "0.85",
+    maxWidth: "100%",
+    maxHeight: "100%",
+    initialWidth: "300",
+    initialHeight: "100",
     overlayClose: true,
     scrolling: true,
-    speed: '500',
-    current: 'Bild {current} von {total}',
-    previous: '&#10094;',
-    next: '&#10095;',
-    close: '&times;',
-    xhrError: 'Dieser Inhalt konnte nicht geladen werden.',
-    imgError: 'Dieses Bild konnte nicht geladen werden.',
-    title: function() { return $(this).children('img').attr('alt'); },
+    speed: "500",
+    current: "Bild {current} von {total}",
+    previous: "&#10094;",
+    next: "&#10095;",
+    close: "&times;",
+    xhrError: "Dieser Inhalt konnte nicht geladen werden.",
+    imgError: "Dieses Bild konnte nicht geladen werden.",
+    title: function() {
+      return $(this)
+        .children("img")
+        .attr("alt")
+    },
     returnFocus: false
-  });
+  })
   */
   /**
    * Handles mailing and animation
    *
    */
   /*
-  // submit function
-  $("#contact form").submit(function(e){
-    e.preventDefault();
-    var $form = $(this);
-    var $responseContainer = $("#response");
+  $("#contact form").submit(function(e) {
+    e.preventDefault()
+    const $form = $(this)
+    const $responseContainer = $("#response")
 
-    $responseContainer.addClass("hidden");
+    $responseContainer.addClass("hidden")
 
-    if ($form.children().eq(0).val() == ""
-        || $form.children().eq(2).val() == ""
-        || $form.children().eq(3).val() == "") {
+    const $name = $form.children().eq(0)
+    const $email = $form.children().eq(1)
+    const $message = $form.children().eq(2)
 
-      $responseContainer.html("&rarr; Füllen Sie alle Felder aus.");
-      $responseContainer.removeClass("hidden");
-      return;
+    if ($name.val() === "" || $email.val() === "" || $message.val() === "") {
+      $responseContainer.html("Füllen Sie alle Felder aus.")
+      $responseContainer.removeClass("hidden")
+      return
     }
 
-    $form.addClass("hidden");
+    $form.addClass("hidden")
 
     $.ajax({
       type: "POST",
-      url: $(this).attr("action"),
-      data: $(this).serialize()
+      url: $form.attr("action"),
+      data: $form.serialize()
     })
-    .done(function(response) {
-      $responseContainer.html("&rarr; Die Nachricht wurde erfolgreich gesendet.");
-      $responseContainer.removeClass("hidden");
-    })
-    .fail(function(response) {
-      $responseContainer.html("&rarr; Es ist ein Fehler aufgetreten.");
-      $responseContainer.removeClass("hidden");
-      $form.removeClass("hidden");
-    });
-  });
+      .then(function() {
+        $responseContainer.html("Die Nachricht wurde erfolgreich gesendet.")
+      })
+      .catch(function() {
+        $responseContainer.html("Es ist ein Fehler aufgetreten.")
+        $form.removeClass("hidden")
+      })
+      .always(function() {
+        $responseContainer.removeClass("hidden")
+      })
+  })
 
-  // Remove error/notification messages on reset
-  $("#contact form input[type='reset']").click(function(e) {
-    $("#response").addClass("hidden");
-  });
+  $formContainer.find("input[type='reset']").click(function() {
+    $responseContainer.addClass("hidden")
+  })
   */
 })
